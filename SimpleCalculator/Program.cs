624409b27814348bb5b57c9dc42e12519fddc091
kndynow <139381069+kndynow@users.noreply.github.com>
2024-10-02 10:49:57 +0200

    Initial commit: add project files
    Add project files with original code for a simple calculater.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SimpleCalculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enkel Kalkylator");
+        Console.WriteLine("Ange operation (+, -, *, /): ");
+        char operation = Console.ReadKey().KeyChar;
+        Console.WriteLine("\nAnge första talet: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("\nAnge andra talet: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Ogiltig operation.");
+                break;
+        }
+        Console.WriteLine($"Resultat: {result}");
+        Console.ReadLine();
+    }
+}
+
